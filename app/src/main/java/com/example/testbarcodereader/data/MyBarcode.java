f7fbba1a83b44feceb4722cc8c4bb5a25e4a8179
2wiktor2 <wiktor2011@gmail.com>
2020-07-16 11:40:23 +0300

    dev_16_07_20 commit first

--- app/src/main/java/com/example/testbarcodereader/data/MyBarcode.java
+++ app/src/main/java/com/example/testbarcodereader/data/MyBarcode.java
@@ -1,10 +1,15 @@
 package com.example.testbarcodereader.data;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class MyBarcode implements Serializable {
+    @SerializedName("barcodeResult")
     private String barcodeResult;
+    @SerializedName("amountOfNumbers")
     private int amountOfNumbers;
+    @SerializedName("amountOfLetters")
     private int amountOfLetters;
 
     public MyBarcode(String barcodeResult, int amountOfNumbers, int amountOfLetters) {
