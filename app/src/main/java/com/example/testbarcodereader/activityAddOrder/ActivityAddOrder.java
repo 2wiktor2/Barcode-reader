5f77c53409d1fcdf1934e5495d537d4d2f268b96
lomov <wiktor2BK.RU>
2020-07-27 15:20:48 +0300

    новая главная активити

--- app/src/main/java/com/example/testbarcodereader/activityAddOrder/ActivityAddOrder.java
+++ app/src/main/java/com/example/testbarcodereader/activityAddOrder/ActivityAddOrder.java
@@ -1,16 +1,22 @@
-package com.example.testbarcodereader.activityAddRequest;
+package com.example.testbarcodereader.activityAddOrder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.testbarcodereader.R;
 
-public class ActivityAddRequest extends AppCompatActivity {
+public class ActivityAddOrder extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_request);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
