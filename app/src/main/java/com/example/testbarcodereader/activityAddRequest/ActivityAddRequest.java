d922707878e52c7944656f87ec205e2921d40a4c
lomov <wiktor2BK.RU>
2020-07-27 14:56:59 +0300

    firebase Crashlytics не запускаеится

---
+++ app/src/main/java/com/example/testbarcodereader/activityAddRequest/ActivityAddRequest.java
@@ -0,0 +1,16 @@
+package com.example.testbarcodereader.activityAddRequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.testbarcodereader.R;
+
+public class ActivityAddRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_request);
+    }
+}
